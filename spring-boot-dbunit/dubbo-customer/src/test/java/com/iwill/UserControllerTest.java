f1c316651f6d3e9874f72d14a0fcc4ef92329e0b
iwill <yangjianzhouctgu@126.com>
2018-06-03 10:41:54 +0800

    add mockito

--- spring-boot-dbunit/dubbo-customer/src/test/java/com/iwill/UserControllerTest.java
+++ spring-boot-dbunit/dubbo-customer/src/test/java/com/iwill/UserControllerTest.java
@@ -5,9 +5,13 @@ import com.github.springtestdbunit.annotation.DatabaseOperation;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.DbUnitConfiguration;
 import com.iwill.controller.UserController;
+import com.iwill.service.UserService;
+import com.iwill.service.api.IProductService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.internal.matchers.Any;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -16,6 +20,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = CustomerApplication.class)
@@ -30,9 +35,16 @@ public class UserControllerTest {
     @Autowired
     private UserController userController;
 
+    @Autowired
+    private UserService userService;
+
     @Test
-    @DatabaseSetup(value = {"/data/getuserInfo.xls"}, type = DatabaseOperation.CLEAN_INSERT)
+    @DatabaseSetup(value = {"/data/getUserInfo.xls"}, type = DatabaseOperation.CLEAN_INSERT)
     public void test() {
+        IProductService productService = Mockito.mock(IProductService.class);
+        Mockito.when(productService.getProductName(Mockito.anyLong())).thenReturn("mock");
+        ReflectionTestUtils.setField(userService, "productService", productService);
+
         String result = userController.getUserInfo("1234");
         Assert.assertTrue(result.equals("SUCCESS"));
     }
